1bfadd0e4cf7483e1db9605ae05ff19cdfa95306
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-04-26 15:38:18 -0400

    Increased coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,10 +7,18 @@ from click.testing import CliRunner
 
 @pytest.fixture
 def simple_report():
+    """Example of parsed pytest-json report contents."""
     current_folder = pathlib.Path(__file__).resolve().parent
     return json.loads((current_folder / "simple_report.json").read_text())
 
 
 @pytest.fixture
+def converted_tests():
+    """Example of pytest-json report output converted to Mig3 spec."""
+    return [{"module": "tests/test_converter.py", "test": "test_simple_convert"}]
+
+
+@pytest.fixture
 def cli_runner():
+    """Create a Click CLI runner."""
     return CliRunner()
--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -6,7 +6,28 @@ def test_simple_report(simple_report):
     assert len(simple_report["included"]) == 1
 
 
-def test_simple_convert(simple_report):
-    """Should build submission from simplest possible report."""
+def test_basic_convert(simple_report):
+    """Should convert tests from simplest possible report."""
     converter = ReportConverter(simple_report)
     assert len(converter.convert()) == 1
+
+
+def test_convert_module_name(simple_report):
+    """Should extract module name from report item."""
+    result = ReportConverter(simple_report).convert()
+    converted_test = result[0]
+    assert converted_test.get("module") == "tests/test_examples.py", converted_test
+
+
+def test_convert_test_name(simple_report):
+    """Should extract test name from report item."""
+    result = ReportConverter(simple_report).convert()
+    converted_test = result[0]
+    assert converted_test.get("test") == "test_success", converted_test
+
+
+def test_convert_outcome(simple_report):
+    """Should extract outcome from report item."""
+    result = ReportConverter(simple_report).convert()
+    converted_test = result[0]
+    assert converted_test.get("outcome") == "passed", converted_test
